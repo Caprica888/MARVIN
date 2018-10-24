cbdbc0997c1a5b81a7a62503164250a2f140d4df
AssassinOverkill <tyler.shamon@uoit.net>
2018-10-24 19:35:38 -0400

    UI Hologram v1.0
    +UI hologram plane with text spawns in
    +can change text (ONLY 6 LINES ALLOWED)
    +can change plane and text color
    +spawns on a set spawn point (can just use an empty gameobject). Be mindful that the origin is in the middle of the plane

---
+++ Assets/Scripts/UIHologram_ObjectScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHologram_ObjectScript : MonoBehaviour {
+    #region variables
+    public List<string> lines = new List<string>();
+    public GameObject UI_HologramElement;   //  the UI hologram prefab   
+    public Transform spawnPoint;   //  where the UI will spawn (choose wisely)
+    public Color planeColor;
+    public Color textColor;
+
+    private GameObject UIClone;  //  instantiated UI element
+    #endregion
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))    //  if player triggers with object, spawn UI and set lines
+        {
+            spawnUI();
+        }
+    }
+
+    private void spawnUI()
+    {
+            UIClone = Instantiate(UI_HologramElement, spawnPoint);
+            UIClone.transform.SetParent(this.transform);    //  set this object as parent to the instantiated UI element
+            UIClone.GetComponent<UIHologram>().setPlaneColor(planeColor);
+            UIClone.GetComponent<UIHologram>().setTextColor(textColor);
+
+            for (int i = 0; i < lines.Count; i++)
+            {
+                UIClone.GetComponent<UIHologram>().setLines(lines[i], i);
+            }
+    }
+}
