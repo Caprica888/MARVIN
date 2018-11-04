f3da2604c7b4c9bbcace4a44f0798be43356ece3
Tyler Shamon <100555373@oncampus.local>
2018-11-03 23:57:50 -0400

    UI Bug Fix
    +Fixed issue with UI text rendering behind the plane
    +Fixed issue where the plane, during dynamic sizing, was moved below the text (not sure how that happened or if it won't happen again)

--- Assets/Scripts/UI/UIHologram.cs
+++ Assets/Scripts/UI/UIHologram.cs
@@ -9,11 +9,12 @@ public class UIHologram : MonoBehaviour {
     public GameObject plane;    //  holoUI background
     public GameObject UI_HologramExtension; //  scrollbar extension
     public Canvas thisCanvas;
-    public List<GameObject> buttons = new List<GameObject>();
+    public Button closeButton, extendButton;
     public List<string> text = new List<string>();
     public bool isActive = false;   //  is the UI in the world?
     public float displayHeight; //  how high from the spawn point the plane should be
     public float sizeModifier = 1.9f;   //  used to help modify the dynamic size of the holo-plane
+    public float sizeModifierReducer = 0.4f;
 
     private Text displayText;
     private Color planeColor;
@@ -21,14 +22,16 @@ public class UIHologram : MonoBehaviour {
     private float size;
     #endregion
 
-    public void setLines(string s, int index) {
-        //text[index] = s;
-        text.Add(s);
-    }
+    public void setLines(string s, int index) { text.Add(s); }
 
     public void setPlaneColor(Color col) { planeColor = col; }
 
     public void setTextColor(Color col) { textColor = col; }
+
+    private void openExtension()
+    {
+
+    }
 
     private void Start() {
         size = plane.GetComponent<RectTransform>().localScale.z * sizeModifier;
@@ -38,8 +41,8 @@ public class UIHologram : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-        GetComponent<Transform>().LookAt(player.transform.position);  //  billboarding
+    private void Update () {
+        this.GetComponent<Transform>().LookAt(player.transform.position);  //  billboarding
 
         if (!isActive)  //  if the UI isn't already displayed, display it
         {          
@@ -47,8 +50,12 @@ public class UIHologram : MonoBehaviour {
             {
                 displayText.text += text[i] + "\n";
                 plane.GetComponent<RectTransform>().localScale += new Vector3(0, 0, size);
-                plane.transform.position -= new Vector3(0, size * (sizeModifier + 0.15f), 0);
+                plane.transform.position += new Vector3(0, size * (sizeModifier - sizeModifierReducer), 0);    //  shift the plane into its proper position. 0.4f is just a number to reduce themodifier
             }
+            /*
+                * issue with plane sizes. they physically change but editor says they're the same size? moving extend button for time being
+                *  extendButton.transform.position -= new Vector3(0, (plane.GetComponent<RectTransform>().localScale.y / 2) + (sizeModifier - sizeModifierReducer), 0);
+            */
             isActive = true;
             displayText.color = textColor;
         }
